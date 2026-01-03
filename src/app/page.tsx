c17efac10c575f56df62e8b7f625919f3ff3fc42
Wira <zmuhamad064@gmail.com>
2026-01-03 15:12:25 +0700

    menampilkan data dari api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,37 @@
-export default function Home() {
+import Link from "next/link";
+import "./globals.css";
+
+// panggil api pakai fetch
+async function getLastPost() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=5"
+  );
+  const post = await res.json();
+  return post;
+  // memunculkan di terminal
+  // console.log(await res.json());
+}
+
+export default async function Home() {
+  const posts = await getLastPost();
   return (
-    <div className="main-heading">
-      <h1>Selamat Datang Di Next JS News</h1>
-      <p className="subtitle">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-        minima vel ullam.
-      </p>
-    </div>
+    <>
+      <div className="main-heading">
+        <h1>Selamat Datang Di Next JS News</h1>
+        <p className="subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+          minima vel ullam.
+        </p>
+      </div>
+      <div className="post-list">
+        {posts.map((post) => (
+          <div className="post-item" key={post.id}>
+            <h2>
+              <Link href="/">{post.title}</Link>
+            </h2>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
